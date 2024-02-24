8c23b80c7466ac64f81b68b15855b8b10807681f
LollosaurusRex <ethanrex1@gmail.com>
2024-02-23 18:02:00 -0600

    Improved GetArg with enum

--- Boids/tsgl/GetArguments.cpp
+++ Boids/tsgl/GetArguments.cpp
@@ -6,73 +6,78 @@
 #include <stdio.h>
 #include "GetArguments.hpp"
 
-// int args
-#define WIDTH 0   // Width of plot in pixels
-#define HEIGHT 1  // Height of plot in pixels
-#define NUM 2     // Number of boids.
-#define THREADS 3 // Number of threads
-#define STEPS 4   // Number of simulated steps
-#define SEED 5    // Random seed for initial state
+enum argType : int8_t 
+{
+    // int args
+    width,      // Width of plot in pixels
+    height,     // Height of plot in pixels
+    num,        // Number of boids.
+    threads,    // Number of threads
+    steps,      // Number of simulated steps
+    seed,       // Random seed for initial state
 
-// float args
-#define ANGLE 6  // Number of viewing degrees
-#define VANGLE 7 // Visual avoidance angle
-#define RCOPY 8  // Radius for copy vector
-#define RCENT 9  // Radius for centroid vector
-#define RVOID 10 // Radius for avoidance vector
-#define RVISO 11 // Radius for visual avoidance vector
-#define WCOPY 12 // Weight for copy vector
-#define WCENT 13 // Weight for centroid vector
-#define WVOID 14 // Weight for avoidance vector
-#define WVISO 15 // Weight for visual avoidance vector
-#define DT 16    // Time-step increment
-#define DDT 17   // Momentum factor (0 < ddt < 1)
-#define MINV 18  // Minimum velocity
+    // float args
+    angle,      // Number of viewing degrees
+    vangle,     // Visual avoidance angle
+    rcopy,      // Radius for copy vector
+    rcent,      // Radius for centroid vector
+    rvoid,      // Radius for avoidance vector
+    rviso,      // Radius for visual avoidance vector
+    wcopy,      // Weight for copy vector
+    wcent,      // Weight for centroid vector
+    wvoid,      // Weight for avoidance vector
+    wviso,      // Weight for visual avoidance vector
+    dt,         // Time-step increment
+    ddt,        // Momentum factor (0 < ddt < 1)
+    minv,       // Minimum velocity
 
-#define NO_DRAW 19 // whether to draw on canvas or simulate for speed test
-
-#define HELP 20
+    no_draw,    // whether to draw on canvas or simulate for speed test
+    help
+};
 
 /**
- * @brief Get the arguments from the user's input and modify their parameters
+ * @brief Get the arguments from the user's input and modify their parameters.
+ * 
+ * No checking on bounds is done.
  *
  * @param argc
  * @param argv
- * @param p
+ * @param p a reference to the param struct for modification
+ * @param noDraw a reference to a boolean on whether the screen will be drawn
  */
 void get_arguments(int argc, char *argv[], boids::Params &p, bool& noDraw)
 {
     option longopts[] = {
-        {"width", required_argument, nullptr, WIDTH},
-        {"height", required_argument, nullptr, HEIGHT},
-        {"num", required_argument, nullptr, NUM},
-        {"threads", required_argument, nullptr, THREADS},
-        {"steps", required_argument, nullptr, STEPS},
-        {"seed", required_argument, nullptr, SEED},
-        {"angle", required_argument, nullptr, ANGLE},
-        {"vangle", required_argument, nullptr, VANGLE},
-        {"rcopy", required_argument, nullptr, RCOPY},
-        {"rcent", required_argument, nullptr, RCENT},
-        {"rvoid", required_argument, nullptr, RVOID},
-        {"rviso", required_argument, nullptr, RVISO},
-        {"wcopy", required_argument, nullptr, WCOPY},
-        {"wcent", required_argument, nullptr, WCENT},
-        {"wvoid", required_argument, nullptr, WVOID},
-        {"wviso", required_argument, nullptr, WVISO},
-        {"dt", required_argument, nullptr, DT},
-        {"ddt", required_argument, nullptr, DDT},
-        {"minv", required_argument, nullptr, MINV},
-        {"noDraw", no_argument, nullptr, NO_DRAW},
-        {"help", no_argument, nullptr, HELP},
+        {"width", required_argument, nullptr, argType::width},
+        {"height", required_argument, nullptr, argType::height},
+        {"num", required_argument, nullptr, argType::num},
+        {"threads", required_argument, nullptr, argType::threads},
+        {"steps", required_argument, nullptr, argType::steps},
+        {"seed", required_argument, nullptr, argType::seed},
+        {"angle", required_argument, nullptr, argType::angle},
+        {"vangle", required_argument, nullptr, argType::vangle},
+        {"rcopy", required_argument, nullptr, argType::rcopy},
+        {"rcent", required_argument, nullptr, argType::rcent},
+        {"rvoid", required_argument, nullptr, argType::rvoid},
+        {"rviso", required_argument, nullptr, argType::rviso},
+        {"wcopy", required_argument, nullptr, argType::wcopy},
+        {"wcent", required_argument, nullptr, argType::wcent},
+        {"wvoid", required_argument, nullptr, argType::wvoid},
+        {"wviso", required_argument, nullptr, argType::wviso},
+        {"dt", required_argument, nullptr, argType::dt},
+        {"ddt", required_argument, nullptr, argType::ddt},
+        {"minv", required_argument, nullptr, argType::minv},
+        {"noDraw", no_argument, nullptr, argType::no_draw},
+        {"help", no_argument, nullptr, argType::help},
         {0}};
 
 
-    int c;
+    argType c;
     while (true)
     {
         int option_index = 0;
 
-        c = getopt_long_only(argc, argv, "", longopts, &option_index);
+        c = (argType) getopt_long_only(argc, argv, "", longopts, &option_index);
 
         if (c == -1)
         {
@@ -82,67 +87,67 @@ void get_arguments(int argc, char *argv[], boids::Params &p, bool& noDraw)
         // Highly repetitive but easy to understand.
         switch (c)
         {
-        case WIDTH:
+        case argType::width:
             p.width = atoi(optarg);
             break;
-        case HEIGHT:
+        case argType::height:
             p.height = atoi(optarg);
             break;
-        case NUM:
+        case argType::num:
             p.num = atoi(optarg);
             break;
-        case THREADS:
+        case argType::threads:
             p.threads = atoi(optarg);
             break;
-        case STEPS:
+        case argType::steps:
             p.steps = atoi(optarg);
             break;
-        case ANGLE:
+        case argType::angle:
             p.angle = atof(optarg);
             break;
-        case VANGLE:
+        case argType::vangle:
             p.vangle = atof(optarg);
             break;
-        case RCOPY:
+        case argType::rcopy:
             p.rcopy = atof(optarg);
             break;
-        case RCENT:
+        case argType::rcent:
             p.rcent = atof(optarg);
             break;
-        case RVOID:
+        case argType::rvoid:
             p.rvoid = atof(optarg);
             break;
-        case RVISO:
+        case argType::rviso:
             p.rviso = atof(optarg);
             break;
-        case WCOPY:
+        case argType::wcopy:
             p.wcopy = atof(optarg);
             break;
-        case WCENT:
+        case argType::wcent:
             p.wcent = atof(optarg);
             break;
-        case WVOID:
+        case argType::wvoid:
             p.wvoid = atof(optarg);
             break;
-        case WVISO:
+        case argType::wviso:
             p.wviso = atof(optarg);
             break;
-        case DT:
+        case argType::dt:
             p.dt = atof(optarg);
             break;
-        case DDT:
+        case argType::ddt:
             p.ddt = atof(optarg);
             break;
-        case MINV:
+        case argType::minv:
             p.minv = atof(optarg);
             break;
-        case SEED:
+        case argType::seed:
             p.seed = atof(optarg);
             break;
-        case NO_DRAW:
+        case argType::no_draw:
             noDraw = true;
             break;
-        case HELP:
+        case argType::help:
             // Long print explaining 
             print_help();
             exit(0);
